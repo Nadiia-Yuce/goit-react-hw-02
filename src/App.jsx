23cb23b67e53a7547b1883d72f8ad7d7c25c32af
Nadiia-Yuce <soyka9616@gmail.com>
2024-08-14 23:47:58 +0200

    add localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,39 @@ import Description from "./components/Description/Description";
 import Feedback from "./components/Feedback/Feedback";
 import Options from "./components/Options/Options";
 import Notification from "./components/Notification/Notification";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import css from "./App.module.css";
 
-export default function App() {
-  const [options, setOptions] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+//Функція для отримання значеня з локал стореджу (або надавання дефолтного) як початкового для стану компоненту
+function getInitialOptions() {
+  const savedOptions = window.localStorage.getItem("savedOptions");
+  return savedOptions !== null
+    ? JSON.parse(savedOptions)
+    : {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      };
+}
 
+export default function App() {
+  //Оголошення стану, який буде локально фіксувати зміни з початковим значенням - результатом фиконання функції
+  const [options, setOptions] = useState(getInitialOptions);
+
+  //Записуємо актуальний стан компонента після оновлення в локал сторедж
+  useEffect(() => {
+    window.localStorage.setItem("savedOptions", JSON.stringify(options));
+  }, [options]);
+
+  //Підрахунок загальної кількості фідбеків
   const totalFeedback = options.good + options.neutral + options.bad;
 
+  //При зміні властивості об'єкту або масиву преба обов'язково робити копію (через ...) для уникнення мутації і втрати значень
   function updateFeedback(feedbackType) {
     setOptions({ ...options, [feedbackType]: options[feedbackType] + 1 });
   }
 
+  //Функція скидання значень фідбеку
   function resetFeedback() {
     setOptions({
       good: 0,
